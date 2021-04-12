9a8f66eab4dc4e6eea010b4322985dec3727d377
Jeff Madrelejos <uriel-j.madrelejos@mynt.xyz>
2021-04-12 17:17:37 +0800

    Added @RequestMapping parameters on finance controller

--- api-gateway/src/main/java/ph/apper/gateway/FinanceController.java
+++ api-gateway/src/main/java/ph/apper/gateway/FinanceController.java
@@ -6,12 +6,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import ph.apper.gateway.payload.AddMoneyRequest;
 import ph.apper.gateway.payload.TransferRequest;
 
 import java.util.Map;
 
+@RestController
+@RequestMapping
 public class FinanceController {
     private static final Logger LOGGER = LoggerFactory.getLogger(FinanceController.class);
 
