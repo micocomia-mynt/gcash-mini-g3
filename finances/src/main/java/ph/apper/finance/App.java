3111870a3e99bc7716e0ab83def4c739accbe560
Jeff Madrelejos <uriel-j.madrelejos@mynt.xyz>
2021-04-12 22:55:10 +0800

    Modified database endpoint in application.properties

--- finances/src/main/java/ph/apper/finance/App.java
+++ finances/src/main/java/ph/apper/finance/App.java
@@ -7,6 +7,7 @@ import org.springframework.boot.context.ApplicationPidFileWriter;
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
+        System.setProperty("java.net.preferIPv4Stack", "true");
         SpringApplication app = new SpringApplication(App.class);
         app.addListeners(new ApplicationPidFileWriter("finance_process.pid"));
         app.run(args);
